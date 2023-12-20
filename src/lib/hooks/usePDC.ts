9853cf54a5bf9bec91b153889af677ac36054856
8dong <hyde981114@gmail.com>
2023-12-20 10:42:58 +0900

    fix: Fix usePDC delay milliseconds (#48)

--- src/lib/hooks/usePDC.ts
+++ src/lib/hooks/usePDC.ts
@@ -2,14 +2,14 @@ import { useContext } from 'react'
 import { ReactStackContext } from '../componets/provider'
 
 export function usePDC() {
-  const { isPDC, setPDC } = useContext(ReactStackContext)
+  const { isPDC, setPDC, animationDuration, animationDelay } = useContext(ReactStackContext)
 
   const handleClick = (fnc: any) => {
     if (isPDC) return
     setPDC(true)
     setTimeout(() => {
       setPDC(false)
-    }, 300)
+    }, animationDuration + animationDelay)
     fnc()
   }
 
